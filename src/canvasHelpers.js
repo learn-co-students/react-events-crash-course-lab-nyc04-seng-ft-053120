6610424ea2560d4c5a1de655aa39125cc4dec647
tracycollins <tc@threeceemedia.com>
2020-08-25 16:47:52 -0400

    Done.

--- src/canvasHelpers.js
+++ src/canvasHelpers.js
@@ -8,7 +8,7 @@ let idx = 0
 let [sizeX, sizeY] = [95, 121]
 
 export function init() {
-  
+
   def = document.createElement("img")
   const green = document.createElement("img")
   const red = document.createElement("img")
@@ -18,17 +18,17 @@ export function init() {
   green.src = "chrome-boi-green.png"
   red.src = "chrome-boi-red.png"
   yellow.src = "chrome-boi-yellow.png"
-  
+
   colors = [green, red, yellow]
+  // debugger
 }
 
 export function drawChromeBoiAtCoords(x, y) {
-  
   const canvas = document.querySelector("canvas") // sloppy but we haven't introduced lifecycle methods and canvas wouldn't be rendered
   const ctx = canvas.getContext("2d")
   const rect = canvas.getBoundingClientRect()
   const [cX, cY] = [rect.left, rect.top]
-  
+
   let img
   if (cycling) {
     img = colors[idx]
@@ -36,8 +36,10 @@ export function drawChromeBoiAtCoords(x, y) {
   } else {
     img = def
   }
-  
+
+  console.log(x, y)
   ctx.drawImage(img, x - cX - 50, y - cY - 80, sizeX, sizeY)
+  // ctx.drawImage(img, x, y, sizeX, sizeY)
 }
 
 export function toggleCycling() {
@@ -45,6 +47,7 @@ export function toggleCycling() {
 }
 
 export function resize(type) {
+
   const multiplier = (type === "+") ? 1.1 : 0.9
   sizeX *= multiplier
   sizeY *= multiplier
